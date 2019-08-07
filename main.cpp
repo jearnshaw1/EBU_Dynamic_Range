8ea92525e45442f69d9201d20a3a69e92ef87661
jearnshaw1 <43389573+jearnshaw1@users.noreply.github.com>
2019-08-07 22:04:49 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,11 +7,11 @@
 
 int main(int argc, char* argv[])
 {
-if( argc==0  ) {
+if( argc==1  ) {
       printf("The path to a wave file was not specified\n");
       return 1;
    }
-   else if( argc > 1 ) {
+   else if( argc > 2 ) {
       printf("Too many arguments supplied.\n");
       return 1;
    }
