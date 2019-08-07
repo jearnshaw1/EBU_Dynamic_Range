96339432e29a98c434699f7624eb58b48e555fad
jearnshaw1 <43389573+jearnshaw1@users.noreply.github.com>
2019-08-07 21:59:38 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include "functions.h"
+#include "stdio.h"
+
+
+
+
+
+int main(int argc, char* argv[])
+{
+if( argc==0  ) {
+      printf("The path to a wave file was not specified\n");
+      return 1;
+   }
+   else if( argc > 1 ) {
+      printf("Too many arguments supplied.\n");
+      return 1;
+   }
+   
+   waveloader waveloader1(argv[0]);
+   R128(waveloader1&);
+   
+return 0;
+}
